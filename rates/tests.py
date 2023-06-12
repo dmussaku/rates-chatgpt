8f2e1f88e93ad46947688430268ee8a735881d8f
dmussaku <dmussaku@gmail.com>
2023-06-12 08:10:24 +0200

    Add requirements

--- rates/tests.py
+++ rates/tests.py
@@ -1,1 +1,15 @@
 from django.test import TestCase
+
+
+# generate me a test case for the rate model
+class RateTestCase(TestCase):
+    def setUp(self):
+        Rate.objects.create(
+            user_id=1,
+            movie_id=1,
+            rate=5
+        )
+
+    def test_rate(self):
+        rate = Rate.objects.get(user_id=1)
+        self.assertEqual(rate.rate, 5)
